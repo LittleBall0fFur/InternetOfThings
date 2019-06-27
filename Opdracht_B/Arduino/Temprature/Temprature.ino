9ca0c22d6fc08b1099ea9704ba5d9718874c7874
Naomi Poort <naomi.roberta.ans.poort@gmail.com>
2019-06-27 15:59:06 +0200

    Circuit scheme upload

--- Opdracht_B/Arduino/Temprature/Temprature.ino
+++ Opdracht_B/Arduino/Temprature/Temprature.ino
@@ -4,10 +4,12 @@ const int sensor=A5; // Assigning analog pin A5 to variable 'sensor'
 
 float tempc; //variable to store temperature in degree Celsius
 
+float tempf; //variable to store temperature in Fahreinheit
+
 float vout; //temporary variable to hold sensor reading
 
 void setup() {
-
+  
   pinMode(sensor,INPUT); // Configuring sensor pin as input
   
   Serial.begin(9600);
@@ -15,7 +17,7 @@ void setup() {
 }
 
 void loop() {
-
+    
   vout = analogRead(sensor); //Reading the value from sensor
   
   vout = (vout * 500) / 1023;
@@ -28,5 +30,7 @@ void loop() {
   
   Serial.print(tempc);
   
-  delay(500);
+  Serial.println();
+  
+  delay(500); //Delay of 1 second for ease of viewing 
 }
